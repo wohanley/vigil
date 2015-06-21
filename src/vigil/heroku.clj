514bad5b28f14149b735c2d1e6c316a5c51571e2
William O'Hanley <liam@wohanley.com>
2015-06-21 01:17:04 -0300

    document convert-database-url

--- src/vigil/heroku.clj
+++ src/vigil/heroku.clj
@@ -1,6 +1,9 @@
 (ns vigil.heroku)
 
 (defn convert-database-url [db-url]
+  "Turn a database URL into a JDBC-style connection spec. Heroku likes its DB
+  specs as URL strings stored in environment variables, so that's how we'll do
+  it."
   (let [[_ user password host port db] (re-matches #"postgres://(?:(.+):(.*)@)?([^:]+)(?::(\d+))?/(.+)" db-url)]
     {
       :user user
